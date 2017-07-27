bfdfb874444ef572ce073e9a9e4e66f1ad3b111a
Alex <okuznietsov@warfare-tec.com>
2017-07-27 10:15:20 +0300

    refactored

--- src/component/List.js
+++ src/component/List.js
@@ -9,9 +9,6 @@ class List extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      users: props.users
-    }
     that = this
   }
 
@@ -24,7 +21,7 @@ class List extends React.Component {
 
 const renderItem = (user) => <ListGroupItem href='#' key={user.id}>{user.name}
                               <Button className='delete_btn'
-                                      onClick={that.props.deleteUser.bind(that, user.id)}
+                                      onClick={(event) => that.props.deleteUser(user.id)}
                                       bsStyle='danger'
                                       bsSize='xsmall'><Glyphicon glyph='remove'/></Button>
                             </ListGroupItem>
